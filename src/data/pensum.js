79d4f212629886333af73e765ec5aa609f88eb0a
kernel503 <juanko682@gmail.com>
2020-12-17 20:02:19 -0600

    Primer Commit 🎉

---
+++ src/data/pensum.js
@@ -0,0 +1,32 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default [
+    {
+        ciclo: 1,
+        materias: [
+            {
+                abreviatura: 'MTE115',
+                nombreMateria: 'Métodos experimentales',
+                aprobada: false,
+                requisito: []
+            },
+            {
+                abreviatura: 'MAT115',
+                nombreMateria: 'Matematicas I',
+                aprobada: false,
+                requisito: []
+            },
+            {
+                abreviatura: 'IAI115',
+                nombreMateria: 'Introducción a la Informática',
+                aprobada: false,
+                requisito: []
+            },
+            {
+                abreviatura: 'PSI115',
+                nombreMateria: 'Psicologia Social',
+                aprobada: false,
+                requisito: []
+            },
+        ]
+    },
+];
